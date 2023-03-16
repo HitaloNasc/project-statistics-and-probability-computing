c3a19edf053ac9b87e60007c3844f7f2997cda57
hitalonasc <hitalo.bruno@salvus.me>
2023-03-15 23:35:46 -0300

    create src structure

---
+++ src/modules/graphics.py
@@ -0,0 +1,15 @@
+"""
+This module contains generation functions for graphs
+"""
+
+# function exemple:
+"""
+Responsability
+
+Args:
+    parametro1: O primeiro parâmetro, um número inteiro.
+    parametro2: O segundo parâmetro, um número inteiro.
+
+Retruns:
+    Um número inteiro representando a soma dos parâmetros.
+"""
---
+++ src/modules/measure_centrality.py
@@ -0,0 +1,15 @@
+"""
+This module contains the centrality measures functions
+"""
+
+# function exemple:
+"""
+Responsability
+
+Args:
+    parametro1: O primeiro parâmetro, um número inteiro.
+    parametro2: O segundo parâmetro, um número inteiro.
+
+Retruns:
+    Um número inteiro representando a soma dos parâmetros.
+"""
