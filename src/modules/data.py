b8abe677be4e4689b41d4e34f1b2c865568bf033
katharianabrahel <katharian.abrahel@gmail.com>
2023-03-18 18:28:21 -0300

    add formatted data file

---
+++ src/modules/data.py
@@ -0,0 +1,29 @@
+"""Esse módulo contém os dados formatados"""
+
+def read_data(path):
+    """
+    Lê o arquivo .txt com os dados
+    Arg:
+        path: endereço do arquivo.
+    Return:
+        Uma variável com o arquivo aberto em modo leitura.
+    """
+    data_set = open(path, 'r')
+    return data_set
+
+def format(data_set):
+    """
+    Formatar arquivo .txt que contém os dados
+    Arg:
+        data_set: variável com os dados em arquivo .txt.
+    Return:
+        Uma lista com dados formatados e no tipo float.
+    """
+    data = []
+    for linha in data_set:
+        data.append(float(linha.replace(',','.')))
+    return data
+
+
+data_set = read_data('docs/data_set_group_0.txt')
+data = format(data_set)
