0094a770b3490772bd4886deac467fc1efad61c7
RenataAndradeSnatana <renatandradesg@yahoo.com.br>
2023-03-21 13:13:20 -0300

    posição_gráfico_corrigida

--- src/modules/graphics.py
+++ src/modules/graphics.py
@@ -11,3 +11,14 @@ def get_boxplot(data):
         Criação gráfica do boxplot a partir dos dados inseridos
     """
     return plt.boxplot(data)
+
+
+def get_histograma(data):
+    """
+    Cria o gráfico histograma
+    Arg: 
+        data: Dados formatados
+    Return:
+        Criação gráfica em histograma a partir dos dados inseridos
+    """
+    return plt.hist(data)
