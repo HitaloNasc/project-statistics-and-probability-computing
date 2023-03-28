c016be15afd3e460d235ae7bdfc38aae8e1e6269
RenataAndradeSnatana <renatandradesg@yahoo.com.br>
2023-03-28 12:12:23 -0300

    função_min_max

---
+++ src/modules/Funcao_min_max.py
@@ -0,0 +1,21 @@
+
+def valor_min(data):
+    """
+    Cria o gráfico boxplot
+    Arg: 
+        data: Dados formatados
+    Return:
+        Criação gráfica do boxplot a partir dos dados inseridos
+    """
+    return min(data)
+
+
+def valor_min(data):
+    """
+    Cria o gráfico boxplot
+    Arg: 
+        data: Dados formatados
+    Return:
+        Criação gráfica do boxplot a partir dos dados inseridos
+    """
+    return min(data)
