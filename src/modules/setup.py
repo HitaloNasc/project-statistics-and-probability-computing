14b1b555e4ef68a0ed3acef4117d75c95269338f
Douglas Silva <thedouglasilva@gmail.com>
2023-03-29 12:02:17 -0300

    fix in requirements

--- src/modules/setup.py
+++ src/modules/setup.py
@@ -5,3 +5,4 @@ def install_dependencies():
     subprocess.run(['pip', 'install', 'numpy'])
     subprocess.run(['pip', 'install', 'scipy'])
     subprocess.run(['pip', 'install', 'matplotlib'])
+    subprocess.run(['pip', 'install', 'seaborn'])
